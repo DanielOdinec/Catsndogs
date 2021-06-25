467050faf2aebabf7d9588285b6aa931d50c0aa7
DanielOdinec <77277993+DanielOdinec@users.noreply.github.com>
2021-06-25 23:49:52 +0300

    Add files via upload

---
+++ Cat-and-Dog/AT/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace AT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listPets = new System.Windows.Forms.ListBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listPetName = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listPets
+            // 
+            this.listPets.FormattingEnabled = true;
+            this.listPets.Location = new System.Drawing.Point(42, 110);
+            this.listPets.Name = "listPets";
+            this.listPets.Size = new System.Drawing.Size(120, 95);
+            this.listPets.TabIndex = 0;
+            this.listPets.SelectedIndexChanged += new System.EventHandler(this.listPets_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pets";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pet Names";
+            // 
+            // listPetName
+            // 
+            this.listPetName.FormattingEnabled = true;
+            this.listPetName.Location = new System.Drawing.Point(399, 150);
+            this.listPetName.Name = "listPetName";
+            this.listPetName.Size = new System.Drawing.Size(120, 95);
+            this.listPetName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listPetName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listPets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listPets;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listPetName;
+    }
+}
+
---
+++ Cat-and-Dog/AT/Form1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AT
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["AT.Properties.Settings.PetsConnectionString"].ConnectionString;
+        }
+        private void PopulatePetsTable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter ("SELECT * FROM PetType",connection)) 
+            {
+                DataTable petTable = new DataTable();
+                adapter.Fill(petTable);
+
+                listPets.DisplayMember = "PetTypeName";
+                listPets.ValueMember = "Id";
+                listPets.DataSource = petTable;
+            }
+        }
+
+        private void PopulateNames()
+        {
+            string query = "SELECT Pet.Name FROM PetType INNER JOIN Pet ON Pet.TypeId = PetType.Id WHERE PetType.Id = @TypeId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@TypeId", listPets.SelectedValue);
+                DataTable petNameTable = new DataTable();
+                adapter.Fill(petNameTable);
+
+                listPetName.DisplayMember = "Name";
+                listPetName.ValueMember = "Id";
+                listPetName.DataSource = petNameTable;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulatePetsTable();
+        }
+
+        private void listPets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateNames();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
